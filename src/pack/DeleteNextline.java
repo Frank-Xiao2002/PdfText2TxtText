04d235fb1cd68cfc87c34cee7becfd1b4ae30a65
Frank-Xiao <Xiao670127@126.com>
2023-12-28 17:06:37 +0800

    add paragraphing functionality

--- src/pack/DeleteNextline.java
+++ src/pack/DeleteNextline.java
@@ -10,13 +10,16 @@ public class DeleteNextline {
         File original = new File("original.txt"), output = new File("output.txt");
         FileReader fileReader = new FileReader(original);
         FileWriter fileWriter = new FileWriter(output, false);
-        int buf = -1, buf2 = -1;
+        int buf, buf2;
         /*13 represents \r, 10 represents \n, read out order is 13,10*/
         while ((buf = fileReader.read()) != -1) {
             System.out.print((char) buf);
             if (buf == 10) {
                 buf2 = fileReader.read();
                 if (buf2 == 13) {
+                    //noinspection ResultOfMethodCallIgnored
+                    fileReader.read();
+                    //noinspection ResultOfMethodCallIgnored
                     fileReader.read();
                     fileWriter.write("\n\n");
                 } else {
